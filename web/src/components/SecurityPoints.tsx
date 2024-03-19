39f3ec047c20df79ed0456ff15e7231604e8203e
miguel-resource <miguelbercru27@gmail.com>
2024-03-19 14:29:37 -0600

    [ADD] security poits static

--- web/src/components/SecurityPoints.tsx
+++ web/src/components/SecurityPoints.tsx
@@ -1,10 +1,76 @@
-
-
-
 export default function SecurityPoints() {
-    return (
-        <section>
-            <h2>SecurityPoints</h2>
-        </section>
-    );
+  return (
+    <section className="mb-10 mt-10 bg-black bg-opacity-80 text-white text-center tracking-widest w-full">
+      <div
+        className="grid grid-cols-2 gap-10 text-sm   
+            sm:grid-cols-6 sm:text-xs sm:w-11/12 lg:text-sm lg:w-10/12 xl:w-7/12  lg:gap-12 w-8/12 mx-auto mt-10"
+      >
+        <div>
+          <img
+            className="w-12/12 mx-auto"
+            src="https://www.sprintcopy.com/wp-content/uploads/2020/05/icon-1.png"
+            alt="Alianza Empresarial"
+          />
+          <h4 className="text-white text-center font-light text-md mt-10 mb-10 tracking-widest ">
+            Seguridad
+          </h4>
+        </div>
+
+        <div className="max-w-xs">
+          <img
+            className="w-12/12 mx-auto"
+            src="https://www.sprintcopy.com/wp-content/uploads/2020/05/icon-1.png"
+            alt="Alianza Empresarial"
+          />
+          <h4 className="text-white text-center font-light text-md mt-10 mb-10 tracking-widest ">
+            Seguridad
+          </h4>
+        </div>
+
+        <div className="max-w-xs">
+          <img
+            className="w-12/12 mx-auto"
+            src="https://www.sprintcopy.com/wp-content/uploads/2020/05/icon-1.png"
+            alt="Alianza Empresarial"
+          />
+          <h4 className="text-white text-center font-light text-md mt-10 mb-10 tracking-widest ">
+            Seguridad
+          </h4>
+        </div>
+
+        <div className="max-w-xs">
+          <img
+            className="w-12/12 mx-auto"
+            src="https://www.sprintcopy.com/wp-content/uploads/2020/05/icon-1.png"
+            alt="Alianza Empresarial"
+          />
+          <h4 className="text-white text-center font-light text-md mt-10 mb-10 tracking-widest ">
+            Seguridad
+          </h4>
+        </div>
+
+        <div className="max-w-xs">
+          <img
+            className="w-12/12 mx-auto"
+            src="https://www.sprintcopy.com/wp-content/uploads/2020/05/icon-1.png"
+            alt="Alianza Empresarial"
+          />
+          <h4 className="text-white text-center font-light text-md mt-10 mb-10 tracking-widest ">
+            Seguridad
+          </h4>
+        </div>
+
+        <div className="max-w-xs">
+          <img
+            className="w-12/12 mx-auto"
+            src="https://www.sprintcopy.com/wp-content/uploads/2020/05/icon-1.png"
+            alt="Alianza Empresarial"
+          />
+          <h4 className="text-white text-center font-light text-md mt-10 mb-10 tracking-widest ">
+            Seguridad
+          </h4>
+        </div>
+      </div>
+    </section>
+  );
 }
