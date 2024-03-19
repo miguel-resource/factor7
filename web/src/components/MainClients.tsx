ed7ec56bbf76b804839380abf98fabe8bf206ea2
miguel-resource <miguelbercru27@gmail.com>
2024-03-19 12:06:49 -0600

    [ADD] mainClients

--- web/src/components/MainClients.tsx
+++ web/src/components/MainClients.tsx
@@ -1,9 +1,48 @@
-
-
 export default function MainClients() {
-    return (
-        <section>
-            <h2>MainClients</h2>
-        </section>
-    );
+  return (
+    <section className="mb-10 mt-10 text-slate-700 lg:px-0">
+      <h2 className="text-center font-semibold text-xl mt-10 mb-10 tracking-widest lg:text-3xl">
+        Clientes que confían en nosotros
+      </h2>
+      <p className="text-center text-xs mb-10 tracking-widest lg:text-lg lg:w-9/12 mx-auto px-12">
+        Realizamos los proyectos de nuestros clientes con la mayor seriedad,
+        custodiando de principio a fin todo el proceso, con la máxima calidad y
+        asegurando un proyecto “llaves en mano”, permitiendo a nuestros clientes
+        que se centren en su negocio.
+      </p>
+
+      <div className="grid grid-cols-2 gap-4 lg:grid-cols-6 lg:gap-8 w-9/12 mx-auto mt-10">
+        <img
+          className="w-10/12 mx-auto lg:w-full opacity-50 hover:opacity-100 ease-in-out transition-all duration-300"
+          src="https://www.grafiquesmogent.com/wp-content/uploads/2020/04/LOGOS-CLIENTES-vecmedical.png"
+          alt="Clientes"
+        />
+        <img
+          className="w-10/12 mx-auto lg:w-full opacity-50 hover:opacity-100 ease-in-out transition-all duration-300"
+          src="https://www.grafiquesmogent.com/wp-content/uploads/2020/04/LOGOS-CLIENTES-vecmedical.png"
+          alt="Clientes"
+        />
+        <img
+          className="w-10/12 mx-auto lg:w-full opacity-50 hover:opacity-100 ease-in-out transition-all duration-300"
+          src="https://www.grafiquesmogent.com/wp-content/uploads/2020/04/LOGOS-CLIENTES-vecmedical.png"
+          alt="Clientes"
+        />
+        <img
+          className="w-10/12 mx-auto lg:w-full opacity-50 hover:opacity-100 ease-in-out transition-all duration-300"
+          src="https://www.grafiquesmogent.com/wp-content/uploads/2020/04/LOGOS-CLIENTES-vecmedical.png"
+          alt="Clientes"
+        />
+        <img
+          className="w-10/12 mx-auto lg:w-full opacity-50 hover:opacity-100 ease-in-out transition-all duration-300"
+          src="https://www.grafiquesmogent.com/wp-content/uploads/2020/04/LOGOS-CLIENTES-vecmedical.png"
+          alt="Clientes"
+        />
+        <img
+          className="w-10/12 mx-auto lg:w-full opacity-50 hover:opacity-100 ease-in-out transition-all duration-300"
+          src="https://www.grafiquesmogent.com/wp-content/uploads/2020/04/LOGOS-CLIENTES-vecmedical.png"
+          alt="Clientes"
+        />
+      </div>
+    </section>
+  );
 }
