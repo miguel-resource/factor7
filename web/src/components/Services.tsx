4ac039c354287749ce9ab32aa6e9bb417e3fb79f
miguel-resource <miguelbercru27@gmail.com>
2024-03-19 17:10:06 -0600

    [FIX] Services Scale

--- web/src/components/Services.tsx
+++ web/src/components/Services.tsx
@@ -8,30 +8,37 @@ export default function Services() {
     {
       source:
         "https://www.grafiquesmogent.com/wp-content/uploads/2020/04/PLV-001a.jpg",
+      link: "/",
     },
     {
       source:
         "https://www.grafiquesmogent.com/wp-content/uploads/2020/04/rollup-001-960x1050.jpg",
+      link: "/",
     },
     {
       source:
-        "	https://www.grafiquesmogent.com/wp-content/uploads/2020/04/tottem-001a-960x1050.jpg",
+        "https://www.grafiquesmogent.com/wp-content/uploads/2020/04/tottem-001a-960x1050.jpg",
+      link: "/",
     },
     {
       source:
         "https://www.grafiquesmogent.com/wp-content/uploads/2020/05/vinilos-001.jpg",
+      link: "/",
     },
     {
       source:
         "https://www.grafiquesmogent.com/wp-content/uploads/2020/04/carpetas-001b.jpg",
+      link: "/",
     },
     {
       source:
         "https://www.grafiquesmogent.com/wp-content/uploads/2020/04/calendario-001b.jpg",
+      link: "/",
     },
     {
       source:
         "https://www.grafiquesmogent.com/wp-content/uploads/2021/04/stopper-impresion-imprenta-barcelona-005.jpg",
+      link: "/",
     },
   ];
   return (
@@ -45,20 +52,20 @@ export default function Services() {
         {photos.map((photo, index) => (
           <a
             key={index}
-            //   href={photo.link}
+              href={photo.link}
             target="_blank"
             rel="noreferrer"
-            className="w-full h-full ease-in-out transition-all duration-300  group cursor-pointer"
+            className="w-full h-full ease-in-out transition-all duration-300 group hover:scale-95 cursor-pointer"
           >
             <img
               src={photo.source}
               alt="Services"
               className="w-full h-full object-cover group-hover:brightness-50"
             />
-{/* 
-<p className="hidden group-hover:block items-center justify-center text-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center transition-all duration-500 ease-in-out transform group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
+
+            <p className="hidden group-hover:block group-hover:brightness-80 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-slate-100 font-semibold text-lg">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p> */}
+            </p>
           </a>
         ))}
       </Masonry>
