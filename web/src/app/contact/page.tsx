003b958470d72b774f3128cb87649424dfaedcf7
miguel-resource <miguelbercru27@gmail.com>
2024-03-20 16:19:52 -0600

    [FIX] RESPONSIVE CONTACT

--- web/src/app/contact/page.tsx
+++ web/src/app/contact/page.tsx
@@ -4,12 +4,21 @@ import { Button, TextareaAutosize, TextField } from "@mui/material";
 export default function ContactPage() {
   return (
     <CommonWrapper>
-      <section className="mb-10 mt-10 text-slate-700 lg:px-0 text-center flex align-middle">
+      <section className="
+      flex-col 
+      mb-10 mt-10 text-slate-700 text-center flex px-96 align-middle justify-center
+      lg:px-0 lg:text-center lg:items-center lg:justify-center lg:flex-row lg:gap-10 lg:mt-20 lg:mb-20
+        ">
         <form
-          className="flex gap-7 flex-col  w-full  px-12"
+          className="flex gap-7 flex-col mx-auto  w-2/12 px-52 mb-10 :w-12/12 lg:mx-auto lg:px-0"
           action=""
           method="post"
-          style={{ width: "36rem", maxWidth: "100%", height: "auto", alignContent: "center", justifyContent: "center" }}
+          style={{
+            width: "48em", 
+            height: "auto", 
+            alignContent: "center", 
+            justifyContent: "center"
+          }}
         >
 
           <TextField
@@ -42,7 +51,9 @@ export default function ContactPage() {
         </form>
 
         {/*  MAP IFRAME */}
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3814.0873190481193!2d-96.71816912378945!3d17.06838101191477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85c7223100000001%3A0x3d0425a8190a4ab9!2sFactor%20Siete%20Dise%C3%B1o%20Publicitario!5e0!3m2!1ses-419!2smx!4v1710969445040!5m2!1ses-419!2smx" width="600" height="450"  loading="lazy" ></iframe>
+        <iframe 
+        className="w-1/2 mx-auto h-96 sm:w-8/12 md:w-9/12 lg:w-full lg:mx-auto lg:px-0"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3814.0873190481193!2d-96.71816912378945!3d17.06838101191477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85c7223100000001%3A0x3d0425a8190a4ab9!2sFactor%20Siete%20Dise%C3%B1o%20Publicitario!5e0!3m2!1ses-419!2smx!4v1710969445040!5m2!1ses-419!2smx" width="600" height="450"  loading="lazy" ></iframe>
 
       </section>
     </CommonWrapper>
